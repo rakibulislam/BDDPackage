a7a8a048ec4f0e33c01ceb7d874b6f9c34a2efc3
Rakibul Islam <rakib1987@gmail.com>
2015-03-23 23:20:02 -0400

    ruby_deep_clone gem added

--- robdd.rb
+++ robdd.rb
@@ -1,3 +1,5 @@
+require "deep_clone"
+
 class ROBDD
 
   attr_accessor :t, :h
@@ -75,12 +77,13 @@ class ROBDD
       puts "Calling v0:"
       puts "i is: #{i.inspect}"
       puts "func: #{func.inspect}"
-      v0 = build_func(function_set_var_val(deep_copy_func(func), i,0,num_of_vars),i+1,num_of_vars)
+
+      v0 = build_func(function_set_var_val(DeepClone.clone(func), i,0,num_of_vars),i+1,num_of_vars)
 
       puts "Calling v1:"
       puts "i is: #{i.inspect}"
       puts "func: #{func.inspect}"
-      v1 = build_func(function_set_var_val(deep_copy_func(func), i,1,num_of_vars),i+1,num_of_vars)
+      v1 = build_func(function_set_var_val(DeepClone.clone(func), i,1,num_of_vars),i+1,num_of_vars)
 
       triple = Triple.new(i,v0,v1)
 
